1bf173d97de2aef7958120bcd21647ee11695a4e
aambhi <a4mbhi@gmail.com>
2018-07-18 09:00:40 +0530

    first commit

---
+++ sidewalk.logic/Database/AffidavitModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sidewalk.Logic.Database
+{
+    public class AffidavitModel
+    {
+        public long AffidavitId { get; set; }
+        public string PropertyId { get; set; }
+        public string PropertyAddress { get; set; }
+        public string OwnerName { get; set; }
+        public string InspectionDate { get; set; }
+        public string Status { get; set; }
+        public int StatusId { get; set; }
+        public string InspectorName { get; set; }
+        public string NewOwner { get; set; }
+        public bool? IsHighlightOwner { get; set; }
+        public DateTime? RequestedDate { get; set; }
+        public string Comments { get; set; }
+    }
+}
